7479aa2af9191a385c3be17fa4086eb156f38f7c
Owner <alex@ubuntu.ubuntu-domain>
2014-11-13 01:37:36 -0500

    added map implementation

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,7 @@
 var lastIntervalStream;
 var lastUrl = "";
+var marker = null;
+var lastData = null;
 
 
 $(function(){
@@ -38,19 +40,59 @@ $("button").click(function() {
     data: {search: searchValue},
     success: function(data){
    	
-		if(data[0].images.standard_resolution.url!=lastUrl){   
+		if((data[0].images.standard_resolution.url!=lastUrl)&&(data[0].location!=null)){   
    
-    		$('#pic-body').prepend("<a href='"+data[0].link+"'><img class='pic' src='"+
-    		data[0].images.standard_resolution.url+"'></img></a>");
+   		if(lastData!=null){
+    			$('#pic-body').prepend("<a href='"+lastData[0].link+"'><img class='pic' src='"+
+    			lastData[0].images.standard_resolution.url+"'></img></a>");
+    		}
+    		lastData = data;
 			lastUrl = data[0].images.standard_resolution.url;
 
+			$('#current-pic').html('<img id="current-pic-image" src=' + data[0].images.standard_resolution.url + '></img>');
+
+			$('#latitude').html("Lat: "+data[0].location.latitude);
+			$('#longitude').html("Lon: "+data[0].location.longitude);
+			
+			var position = {lat: data[0].location.latitude, lng: data[0].location.longitude};		
+			
+			map.panTo(position);
+			
+			var pinColor = "FE7569";
+	var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0,0),
+    new google.maps.Point(10, 34));	
+			
+			if(marker!=null){
+				marker.setIcon(pinImage);
+				marker.setZIndex(0);
+			}
+
+			
+//RED -> FE7569
+var pinColor = "44DD22";
+var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0,0),
+    new google.maps.Point(10, 34));		
+    
+			marker = new google.maps.Marker({
+   		 position: position,
+   		 map: map,
+   	    title:"Hello World!",
+   	    icon: pinImage
+			});
+			
+			marker.setZIndex(100);
+			updateLocationInfo(position);
 		}		
 		
     	},
    
     });
    
-	}, 2000);
+	}, 3000);
  
     
 });
---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,55 @@
+ var map;
+
+function initialize() {
+  var mapOptions = {
+    center: { lat: -34.397, lng: 150.644},
+    zoom: 4
+  };
+   map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+
+}
+
+$(document).ready(function () {
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+var latlng = new google.maps.LatLng(-34.397, 150.644);
+updateLocationInfo(latlng);
+
+});
+
+function updateLocationInfo(latlng){
+	
+    geocoder = new google.maps.Geocoder();
+    //var latlng = new google.maps.LatLng(-34.397, 150.644);
+    
+    geocoder.geocode({'latLng': latlng}, function(results, status)
+     {
+         //alert("Else loop1");
+         if (status == google.maps.GeocoderStatus.OK)
+          {
+                 if (results[0])
+                 {
+                     var add= results[0].formatted_address ;
+                     var  value=add.split(",");
+
+                     count=value.length;
+                     country=value[count-1];
+                     state=value[count-2];
+                     city=value[count-3];
+                     //alert("city name is: " + country);
+							$('#location-info').html(city+', '+country);
+                 }
+                 else 
+                 {
+           alert("address not found");
+                 }
+         }
+          else
+         {
+				console.log("FAILED!");
+         }
+     });
+
+};
